14140671fc83cb5314b670fada2a7ce19382c636
clm110 <275298711@qq.com>
2017-10-31 18:34:51 +0800

    todolist

--- src/App.vue
+++ src/App.vue
@@ -1,65 +1,72 @@
 <template>
   <div id="app">
-    <Products />
-    <hr>
-    <h1>{{a}}</h1>
-    <h2>{{b[2]}}111</h2>
-    <h2>{{c}}</h2>
-    <Cart/>    
+    <h1>22</h1>
+    <div class="container">
+      <div class="input-group">
+        <span class="input-group-addon" id="basic-addon1">@</span>
+        <input v-model="content" type="text" class="form-control" placeholder="Username" aria-describedby="basic-addon1">
+        <button @click="add" type="button" class="btn btn-default">添加</button>
+      </div>
+    </div>
+    <div class="container">
+      <ul class="list-group">
+        <li v-for=" t in todos" class="list-group-item">
+          <span @click="jian(t.id)" class="badge">删除</span>
+          {{t.content}}
+        </li>
+      </ul>
+    </div>
   </div>
+ 
 </template>
 
 <script>
-import store from './store'
-import Cart from './components/Cart'
-import Products from './components/Products'
-store.commit('gcadd');
-console.log(store.getters.goodlength)
-store.dispatch('getProducts');
-console.log(store.getters.goodlength)
-let cc = {a:1};
+import 'bootstrap/dist/css/bootstrap.min.css'
+import store from "./store";
+function randomString(len) {
+　　len = len || 32;
+　　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+　　var maxPos = $chars.length;
+　　var pwd = '';
+　　for (let i = 0; i < len; i++) {
+　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+　　}
+　　return pwd;
+}
+
 export default {
   data(){
     return {
-      b:this.$store.state.goods
+      content:'',
+      todos:[
+        {id: 1, content:'1111111111'},
+        {id: 2, content:'222222'},
+        {id: 3, content:'33333333'},
+        {id: 4, content:'444444444444444'},
+       ]
     }
-  },
-  computed:{
-     a(){return this.$store.state.goods[0]},
-     c(){
-       return cc.a
-     }
-  },
-  name: 'app',
-  store,
-  components: {
-    Cart,
-    Products
-  },
-  created(){
-       this.log()
   },
   methods:{
-    getProducts(){
-      this.$store.dispatch('getProducts');
-      setTimeout(function(){
-        cc.a+=1
-      })
+    add(){
+      console.log(this.content);
+      let todo = {
+        id : randomString(32),
+        content:this.content
+      }
+      if(this.content){
+        this.todos.push(todo);     
+      }
     },
-    log(){
-      console.log(this.$store.state.goods,15040)
+    jian(id){
+      let index = '';
+      this.todos.forEach((e,i)=>{if(e.id==id){index = i}});
+      console.log(index);
+      this.todos.splice(index,1);
     }
   }
-}
+};
 </script>
 
 <style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
